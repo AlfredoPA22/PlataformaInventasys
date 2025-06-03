8f0d2214fc529c4b19e8fd224a2374b03fa654d5
alfredo parada <alfredoparadaaguilar@gmail.com>
2025-06-03 18:40:07 -0400

    deploy gpql

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -22,7 +22,7 @@ export function LoginForm({ className }: { className?: string }) {
         </p>
       </div>
 
-      <div className="grid gap-6 mt-4">
+      <div className="grid gap-6 mt-4 justify-center">
         <GoogleLogin
           onSuccess={(credentialResponse) => {
             handleLogin(credentialResponse); // token de Google
