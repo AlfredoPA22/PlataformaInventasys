edc8dcdefa1ed7dd1abacd2715c7ccc2d0200a1b
alfredo parada <alfredoparadaaguilar@gmail.com>
2026-03-25 02:22:06 -0400

    deploy

--- src/graphql/mutations/User.ts
+++ src/graphql/mutations/User.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
-  mutation Mutation($clientId: String!, $credential: String!) {
+  mutation loginLanding($clientId: String!, $credential: String!) {
     loginLanding(
       loginLandingInput: { clientId: $clientId, credential: $credential }
     )
